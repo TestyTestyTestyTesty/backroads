dc10f1e0dc2f95d913ae3682a62d793d40f0c090
ST <31928306+TestyTestyTestyTesty@users.noreply.github.com>
2019-09-28 12:42:33 +0200

    added styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
-import './Layout.css'
+
 import { FaBeer } from 'react-icons/fa';
 const Layout = (props) => {
   return (
